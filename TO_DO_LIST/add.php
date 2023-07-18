48c4690e978b3d3099704dc2e5ee84848759a429
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-07-18 19:43:30 +0600

    commit -m "TO_DO_LIST"

---
+++ TO_DO_LIST/add.php
@@ -0,0 +1,16 @@
+<?php
+    //логика добавления задачи
+    //перенапрелние обратно на главную страницу
+    $task = $_POST['task']; //вытащить значение из поля input
+    $regex = '/^$/';
+
+    if(preg_match_all($regex, $task)){
+        header("Location:/?error=empty_input");
+    }
+    else{
+        require 'config.php';
+        $request = $pdo->prepare('INSERT INTO tasks(task) VALUE (:task);');
+        $request->execute(['task'=>$task]);
+        header("Location:/");
+    }
+?>
