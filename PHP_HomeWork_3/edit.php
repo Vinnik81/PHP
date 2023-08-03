47e86b9a742789016adb5bf3f8d20ef2818510a2
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-08-03 20:55:49 +0600

    commit -m "Project_Vulnerabilities"

--- PHP_HomeWork_3/edit.php
+++ PHP_HomeWork_3/edit.php
@@ -9,12 +9,22 @@
 </head>
 <body>
     <h1>Edit Page</h1>
-<form method="post" action="" enctype="multipart/form-data">
+    <div class="button-container d-flex justify-content-start">
+    <form method="post" action="" enctype="multipart/form-data" class="d-flex justify-content-start">
     <label for="avatar_img" class="form-label">Avatar: <input type="file" class="btn btn-outline-warning" id="avatar_img" name="avatar_img"> </label> <br>
-    <button type="submit" class="btn btn-outline-info"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-save2" viewBox="0 0 16 16">
+    <button type="submit" class="btn btn-outline-info me-2 ms-2" style="height: 44px"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="currentColor" class="bi bi-save2" viewBox="0 0 16 16">
   <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v4.5h2a.5.5 0 0 1 .354.854l-2.5 2.5a.5.5 0 0 1-.708 0l-2.5-2.5A.5.5 0 0 1 5.5 6.5h2V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
 </svg></button>
     </form>
+    <form action="index.php" method="post">
+    <button type="submit" class="btn btn-outline-danger me-2" style="height: 44px">
+				<svg xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                </svg>
+		        </button>
+</form>
+    </div>
 
     <?php
 require_once '/project/PHP/TO_DO_SITE/config.php';
